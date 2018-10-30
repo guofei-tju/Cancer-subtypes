42527d244c18d99e709fa054a45c9d79737c903b
guofei <39693114+guofei-tju@users.noreply.github.com>
2018-10-30 17:29:33 +0800

    Add files via upload

---
+++ CS_main.m
@@ -0,0 +1,31 @@
+load('data.mat')
+%tju cs for bioinformatics
+data1 = gene;
+% data1=gene(list_num,:);
+data2 = isoform;
+data3 = miRNA;
+
+data1 = zscore(data1);
+data2 = zscore(data2);
+data3 = zscore(data3);
+
+
+data3(find(isnan(data3)==1)) = 0;
+
+W1 = kernel_eucl(data1,2);  
+W2 = kernel_eucl(data2,2);  
+W3 = kernel_eucl(data3,2); 
+
+% W1 = zscore(W1);
+% W2 = zscore(W2);
+% W3 = zscore(W3);
+
+
+K = 30;%number of neighbors, usually (10~30)
+alpha = 0.9; %hyperparameter, usually (0.3~0.8)
+T = 20; %Num
+
+W = SKF({W1,W2,W3}, K, T,alpha);
+ 
+C = 5; %The number of clusters
+group = SpectralClustering(W,C);
